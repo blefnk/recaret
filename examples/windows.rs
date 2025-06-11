44e25054937aea63dd6d68acfb1d14c917c16fb5
nazar kornienko 🇺🇦/oss <blefnk@gmail.com>
2025-06-11 14:39:22 +0200

    fix ci build failures
    * Fix CI build by adjusting tokio features and gating examples
    
    * Fix X11 build issues in CI

--- examples/windows.rs
+++ examples/windows.rs
@@ -1,6 +1,13 @@
+#[cfg(target_os = "windows")]
 use recaret::platform::windows;
 
+#[cfg(target_os = "windows")]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     windows::start().await
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {
+    eprintln!("This example only runs on Windows.");
 }
